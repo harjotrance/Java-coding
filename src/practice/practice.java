33d043c5c5b71e265233923cad4200592db7b412
Harjot <harjotsrance@gmail.com>
2021-05-12 21:35:37 +0530

    Practice of data types

---
+++ src/practice/practice.java
@@ -0,0 +1,46 @@
+package practice;
+
+public class practice {
+    public static void main(String[] args) {
+
+        byte myNum = 100;
+        System.out.println(myNum);
+
+        short myShort = 5000;
+        System.out.println(myShort);
+
+        int myInt = 100000;
+        System.out.println(myInt);
+
+        long myLong = 15000000000L;
+        System.out.println(myLong);
+
+        float myFloat = 5.75f;
+        System.out.println(myFloat);
+
+        double myDouble = 19.99d;
+        System.out.println(myDouble);
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);
+        System.out.println(isFishTasty);
+
+        char myGrade = 'B';
+        System.out.println(myGrade);
+
+        char a = 65, b = 66, c = 67;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
+
+ }
+}
