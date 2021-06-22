a79cb9a3fa64d9e1cf9bbdcf45f404985dbaeb2d
Harjot <harjotsrance@gmail.com>
2021-06-22 22:26:46 +0530

    Java_coding_sections

--- src/practice/hi.java
+++ src/practice/hi.java
@@ -2,6 +2,6 @@ package practice;
 
 public class hi {
     public static void main(String[] args) {
-        System.out.println("practice.hello");
+        System.out.println("practice.hi");
     }
 }
