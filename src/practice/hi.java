48e78fe50b4a3f59ab4e5523015a5170ec236e37
Harjot <harjotsrance@gmail.com>
2021-06-22 22:25:10 +0530

    74c67829420fc8c333ffb8e4012b9ef6c8287c7f

--- src/practice/hi.java
+++ src/practice/hi.java
@@ -2,6 +2,6 @@ package practice;
 
 public class hi {
     public static void main(String[] args) {
-        System.out.println("practice.hi");
+        System.out.println("practice.hello");
     }
 }
